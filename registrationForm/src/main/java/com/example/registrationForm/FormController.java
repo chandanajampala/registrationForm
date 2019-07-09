6851c9e3b1f62443526127da412ea0e8e322e8e9
SPYRUS <jampala.c@gmail.com>
2019-07-10 03:45:57 +0530

    added validations and cross origin

--- registrationForm/src/main/java/com/example/registrationForm/FormController.java
+++ registrationForm/src/main/java/com/example/registrationForm/FormController.java
@@ -5,26 +5,28 @@ import java.util.List;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+@CrossOrigin(origins = "http://localhost:4200",maxAge = 3600)
 
 @RestController
 @RequestMapping("/regForm")
 public class FormController {
-	
+    @Autowired
 	registrationService regService;
-    
-    @GetMapping("/grtUsers")
+ 
+    @GetMapping("/getUsers")
     public List<userEntity> getUsers() {
         return regService.getAllUsers();
     }
     @PostMapping("/addUsers")
-    public userEntity createUser(@Valid @RequestBody userEntity user) {
-    	
+    public String createUser(@Valid @RequestBody userEntity user) {
         return regService.addUser(user);
+        
     }
 	  
 }
--- registrationForm/src/main/java/com/example/registrationForm/registrationService.java
+++ registrationForm/src/main/java/com/example/registrationForm/registrationService.java
@@ -1,9 +1,14 @@
 package com.example.registrationForm;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.regex.Pattern;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Service
 public class registrationService {
@@ -14,8 +19,39 @@ public class registrationService {
     	return userRepo.findAll();
     }
     
-    public userEntity addUser(userEntity user) {
-    	
-    	return userRepo.save(user);
+    public String addUser(userEntity user) {
+    	boolean emailFlag=true;
+    	boolean numberFlag=true;
+    	ObjectMapper mapper = new ObjectMapper();
+    	HashMap<String,HashMap<String,String>> result =new HashMap<String,HashMap<String,String>> ();
+    	userEntity savedUser;    	HashMap<String, String> errorCodes = new HashMap<String, String>();
+    	if(!Pattern.matches("^\\w+@[a-zA-Z_]+?\\.[a-zA-Z]{2,3}$", user.getEmailId())) {
+    	        emailFlag=false;
+    	        errorCodes.put("emailId", "Enter valid emailID");
+    	}
+    	if(!Pattern.matches("^\\d{10}$", user.getPhoneNo())) {
+        	numberFlag=false;
+	        errorCodes.put("phoneNo", "Enter valid mobile number");
+
+    	}
+    	if(emailFlag&&numberFlag) {
+try {
+    	savedUser = userRepo.save(user);
+    	HashMap<String, String> usermap = mapper.convertValue(savedUser, HashMap.class);
+    	result.put("userData",usermap);
+}
+catch(Exception e) {
+	errorCodes.put("dbError", "some error occouted in db");
+}
+    	}
+    	result.put("errorData", errorCodes);
+   	 String finalResult="";
+	try {
+		finalResult = mapper.writeValueAsString(result);
+	} catch (JsonProcessingException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+    	return finalResult;
     }
 }
